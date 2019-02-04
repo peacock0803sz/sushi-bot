7dddc3c200be8297ac9d088b23a16703aa3c804a
peacock <peacock0803sz@gmail.com>
2019-02-04 15:37:05 +0900

    :sparkles: get text

--- getlive.py
+++ getlive.py
@@ -20,5 +20,8 @@ def get_live():
     driver.find_element_by_id('input__password').send_keys("bartok0803sz")
     driver.find_element_by_id('login__submit').click()
 
+    live_item = driver.find_element_by_class_name('liveItemTxt').text
+    print(live_item)
+
 
 get_live()
