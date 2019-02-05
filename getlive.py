84120273e07390f5cf0d5152973691dee83ecd1d
peacock <peacock0803sz@gmail.com>
2019-02-06 08:25:27 +0900

    :art: Add an exception handing

--- getlive.py
+++ getlive.py
@@ -22,14 +22,18 @@ def get_live():
 
     retval = []
 
-    for i in range(live_item_count):
-        live_item = driver.find_elements_by_class_name('liveItemTxt')[i].text
-        live_link = driver.find_element_by_xpath(f"""//*[@id='subscribeItemsWrap']
-        /div/div[{live_item_count}]/div/h3/a""").get_attribute('href')
+    if 0 < live_item_count:
+        for i in range(live_item_count):
+            live_item = driver.find_elements_by_class_name('liveItemTxt')[i].text
+            live_link = driver.find_element_by_xpath(f"""//*[@id='subscribeItemsWrap']
+            /div/div[{live_item_count}]/div/h3/a""").get_attribute('href')
 
-        retval.append(live_item)
-        retval.append(live_link)
-        retval.append('\n')
+            retval.append(live_item)
+            retval.append(live_link)
+            retval.append('\n')
+
+    else:
+        retval.append('現在放送中の番組はありません。')
 
     return retval
 
