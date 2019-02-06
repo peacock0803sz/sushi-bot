67d7e0b44c6b9d830defd375a6bbd8c1efd7515e
peacock <peacock0803sz@gmail.com>
2019-02-06 17:03:54 +0900

    :hankey: Change account

--- getlive.py
+++ getlive.py
@@ -16,9 +16,9 @@ def get_live():
     driver.get(login_url)
 
     driver.find_element_by_id('input__mailtel').clear
-    driver.find_element_by_id('input__mailtel').send_keys("claude0803sz@gmail.com")
+    driver.find_element_by_id('input__mailtel').send_keys("claude0803.sz@gmail.com")
     driver.find_element_by_id('input__password').clear
-    driver.find_element_by_id('input__password').send_keys("bartok0803sz")
+    driver.find_element_by_id('input__password').send_keys("YdaUjtF72KyU")
     driver.find_element_by_id('login__submit').click()
 
     # 現在放送中の番組を取得
