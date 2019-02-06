d23ea8a4acf3a2a74d1cf81af99ccc215fc40359
peacock <peacock0803sz@gmail.com>
2019-02-06 14:00:05 +0900

    :pencil2: Upper case

--- getlive.py
+++ getlive.py
@@ -6,7 +6,7 @@ options.add_argument('--headless')
 
 url = 'http://live.nicovideo.jp/my'
 
-if chrome_binary_path: options.binary_location = chrome_binary_path
+if CHROME_BINARY_PATH: options.binary_location = CHROME_BINARY_PATH
 driver = webdriver.Chrome(options=options)
 
 
