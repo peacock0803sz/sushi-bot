e6e2da54c64aa789aa68daedfa62cb9065a5d0df
peacock <peacock0803sz@gmail.com>
2019-02-06 16:55:32 +0900

    :ambulance: Fix login session

--- getlive.py
+++ getlive.py
@@ -4,19 +4,26 @@ from selenium.webdriver.chrome.options import Options
 options = Options()
 options.add_argument('--headless')
 
-url = 'http://live.nicovideo.jp/my'
+login_url = 'https://account.nicovideo.jp/login?next_url=my&site=nicolive'
+my_url = 'http://live.nicovideo.jp/my'
 
 driver = webdriver.Chrome(options=options)
 
 
 def get_live():
-    driver.get(url)
+
+    # ログイン処理
+    driver.get(login_url)
 
     driver.find_element_by_id('input__mailtel').clear
     driver.find_element_by_id('input__mailtel').send_keys("claude0803sz@gmail.com")
     driver.find_element_by_id('input__password').clear
     driver.find_element_by_id('input__password').send_keys("bartok0803sz")
     driver.find_element_by_id('login__submit').click()
+
+    # 現在放送中の番組を取得
+
+    driver.get(my_url)
 
     live_item_count = len(driver.find_elements_by_class_name('liveItemTxt'))
 
