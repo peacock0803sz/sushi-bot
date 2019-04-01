68dec09d12d3bb65538b52f00b69f9cbce2b1f22
peacock <peacock0803sz@gmail.com>
2019-04-02 00:37:56 +0900

    :zap: Refactor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-import time
+import asyncio
 
 import discord
 
@@ -15,6 +15,17 @@ async def on_ready():
     print('I have logged in as {0.user}'.format(client))
 
 
+async def watch(message):
+
+    while(True):
+        lives = ''.join(get_live())
+        if lives != '現在放送中の番組はありません。':
+            await message.channel.send(lives)
+        elif lives == '現在放送中の番組はありません。':
+            print('現在放送中の番組はありません。')
+        await asyncio.sleep(300)
+
+
 @client.event
 async def on_message(message):
 
@@ -26,11 +37,6 @@ async def on_message(message):
         await message.channel.send(lives)
 
     if message.content.startswith('/sushi watch'):
-        for i in range(228):
-            if lives != '現在放送中の番組はありません。':
-                await message.channel.send(lives)
-            elif lives == '現在放送中の番組はありません。':
-                print('現在放送中の番組はありません。')
-            time.sleep(300)
+        client.loop.create_task(watch(message))
 
 client.run(DISCORD_TOKEN)
